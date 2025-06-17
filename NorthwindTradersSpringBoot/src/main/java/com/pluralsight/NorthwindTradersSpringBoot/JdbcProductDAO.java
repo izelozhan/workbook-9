b759d7e58a39a7a654acf72f4a17bb17671354cc
Izel Ozhan <izelozhan@gmail.com>
2025-06-17 14:07:37 -0500

    create jdbc product dao class to implement product dao interface

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/JdbcProductDAO.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/JdbcProductDAO.java
@@ -4,25 +4,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class JdbcProductDAO implements IProductDAO{
+public class JdbcProductDAO implements IProductDAO {
 
     DataSource dataSource;
 
     @Autowired
-    public JdbcProductDao(DataSource dataSource) {
+    public JdbcProductDAO(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     @Override
-    public List<Product> getProducts(){
+    public List<Product> getProducts() {
+        String sql = "SELECT ProductID, Name, Category, Price FROM Products";
+        List<Product> list = new ArrayList<>();
+
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+                list.add(new Product(result.getInt("ProductID"),
+                        result.getString("Name"),
+                        result.getString("Category"),
+                        result.getDouble("Price")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
 
     }
 
     @Override
     public void addProduct(Product product) {
+        String sql = "INSERT INTO Products (ProductID, Name, Category, Price) VALUES (?, ?, ?, ?)";
+        try (Connection conn = dataSource.getConnection()) {
 
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, product.getProductId());
+            ps.setString(2, product.getName());
+            ps.setString(3, product.getCategory());
+            ps.setDouble(4, product.getPrice());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/SimpleProductDAO.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/SimpleProductDAO.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+//@Component
 public class SimpleProductDAO implements IProductDAO {
 
     private final List<Product> productList = new ArrayList<>();
