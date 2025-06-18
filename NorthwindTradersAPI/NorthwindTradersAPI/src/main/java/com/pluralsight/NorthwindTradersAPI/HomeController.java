53891a3f88ad654567dd3b51a9636609f48239d0
Izel Ozhan <izelozhan@gmail.com>
2025-06-18 11:20:11 -0500

    home controller exercise

---
+++ NorthwindTradersAPI/NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -0,0 +1,19 @@
+package com.pluralsight.NorthwindTradersAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(@RequestParam(value = "country", required = false) String country) {
+        if (country != null && !country.isEmpty()) {
+            return "Hello " + country;
+        } else {
+            return "Hello World";
+        }
+    }
+}
